6f9c7d87e3ec6e6109148a47693133b95d2f03ba
Danang ardianto <ardhyanto6629@gmail.com>
2021-03-24 12:13:44 +0700

    bkpm-minggu-3/2

--- workshop-minggu-3/routes/web.php
+++ workshop-minggu-3/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ManagementUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/user', [ManagementUserController::class, 'index']);
+Route::resource('/user', ManagementUserController::class);
